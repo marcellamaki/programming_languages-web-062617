536e4573631f7727fc3b13aac52ea9f5afdca908
Marcella Maki <marcellamaki@gmail.com>
2017-06-25 12:09:25 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,33 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |type, hash|
+    binding.binding.pry
+    hash.each do |name, attributes|
+      binding.pry
+    end
+end
+new_hash
+end
+
+
+
+def reformat_languages(languages)
+  language_attributes = {}
+
+  languages.each do |type, language_hash|
+    language_hash.each do |language, attribute_hash|
+      attribute_hash.each do |attribute, str_value|
+        if language_attributes[language].nil?
+          language_attributes[language] = {}
+        end
+        language_attributes[language][:style] ||= [] #I had to use the solutions for this line and still don't totally understand the syntax even with the explaination given
+        language_attributes[language][:style] << type
+        if language_attributes[language][attribute].nil?
+          language_attributes[language][attribute] = str_value
+        end
+      end
+    end
+  end
+  language_attributes
 end
